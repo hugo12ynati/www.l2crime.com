e266ed86372e140a7de427072e0ac543dbd81ba9
hugo12ynati <47875850+hugo12ynati@users.noreply.github.com>
2019-02-22 04:25:04 +0100

    1
    1

---
+++ cutenews15/cdata/users.db.php
@@ -0,0 +1,2 @@
+<?php die("You don't have access to open this file!"); ?>
+1350399981|1|admin|8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918|Admin|rafkub90@gmail.com|0|0|1350474837|1356343722
